b9855cc23e7cd7785b4c43cab582d4e6a6580513
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2025-04-03 08:57:06 +0530

    Update join_reqs.py

--- database/join_reqs.py
+++ database/join_reqs.py
@@ -21,23 +21,23 @@ class SyDReqs:
         else:
             return False
 
-    async def add_user(self, user_id, first_name, username, date):
+    async def ad_user(self, user_id, first_name, username, date):
         try:
             await self.col.insert_one({"_id": int(user_id),"user_id": int(user_id), "first_name": first_name, "username": username, "date": date})
         except:
             pass
 
-    async def get_user(self, user_id):
+    async def gt_user(self, user_id):
         return await self.col.find_one({"user_id": int(user_id)})
 
-    async def get_all_users(self):
+    async def gt_all_users(self):
         return await self.col.find().to_list(None)
 
-    async def delete_user(self, user_id):
+    async def delet_user(self, user_id):
         await self.col.delete_one({"user_id": int(user_id)})
 
-    async def delete_all_users(self):
+    async def delet_all_users(self):
         await self.col.delete_many({})
 
-    async def get_all_users_count(self):
+    async def gt_all_users_count(self):
         return await self.col.count_documents({})
